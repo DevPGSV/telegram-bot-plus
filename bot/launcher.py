562e744a4caabe3a32246eec2dc66f6b024a85cd
DevPGSV <DevPGSV@gmail.com>
2015-05-31 21:22:25 +0200

    Testing output, general improvements

--- bot/launcher.py
+++ bot/launcher.py
@@ -7,6 +7,7 @@ logger = logger()
 import utils
 import traceback
 import sys
+from TC import TC as TC # And TC again
 #from TC import TC as TC # And TC again
 
 our_id = 0
@@ -14,7 +15,10 @@ pp = pprint.PrettyPrinter(indent=4)
 syncFinished = False
 
 def on_binlog_replay_end():
+  global syncFinished
+  print("SYNC!!!")
   syncFinished = True
+  _TC_OUTPUT_TEST()
   return
 
 def on_get_difference_end():
@@ -45,12 +49,13 @@ def on_msg_receive(msg):
   if not syncFinished:
     print ("RET - 1")
     return;
-  pp.pprint(utils.msg2dict(msg))
+  #pp.pprint(utils.msg2dict(msg))
+  logger.msg(msg)
   if msg.dest.id == our_id: # direct message
     peer = msg.src
   else: # chatroom
     peer = msg.dest
-  if msg.text.startswith("!ping"):
+  if msg.text is not None and msg.text.startswith("!ping"):
     peer.send_msg("PONG!")
   return
 
@@ -71,3 +76,17 @@ tgl.set_on_msg_receive(on_msg_receive)
 tgl.set_on_secret_chat_update(on_secret_chat_update)
 tgl.set_on_user_update(on_user_update)
 tgl.set_on_chat_update(on_chat_update)
+
+
+def _TC_OUTPUT_TEST():
+  logger.log(logger.debug, "Testeando cosas:")
+  logger.log(logger.debug, " Colores:")
+  logger.log(logger.debug, "  Font:")
+  logger.log(logger.debug, "   OSCURO: "+TC.Black+"negro "+TC.Red+"rojo "+TC.Green+"verde "+TC.Yellow+"amarillo "+TC.Blue+"azul "+TC.Purple+"morado "+TC.Cyan+"cyan "+TC.White+"blanco")
+  logger.log(logger.debug, "   CLARO : "+TC.IBlack+"negro "+TC.IRed+"rojo "+TC.IGreen+"verde "+TC.IYellow+"amarillo "+TC.IBlue+"azul "+TC.IPurple+"morado "+TC.ICyan+"cyan "+TC.IWhite+"blanco")
+  logger.log(logger.debug, "  Background:")
+  logger.log(logger.debug, "   OSCURO: "+TC.OnBlack+"negro "+TC.OnRed+"rojo "+TC.OnGreen+"verde "+TC.OnYellow+"amarillo "+TC.OnBlue+"azul "+TC.OnPurple+"morado "+TC.OnCyan+"cyan "+TC.OnWhite+"blanco")
+  logger.log(logger.debug, "   CLARO : "+TC.OnIBlack+"negro "+TC.OnIRed+"rojo "+TC.OnIGreen+"verde "+TC.OnIYellow+"amarillo "+TC.OnIBlue+"azul "+TC.OnIPurple+"morado "+TC.OnICyan+"cyan "+TC.OnIWhite+"blanco")
+  logger.log(logger.debug, " Estilos:")
+  logger.log(logger.debug, "   "+TC.Rst+TC.Bold+"Bold "+TC.Rst+TC.Undr+"Undr "+TC.Rst+TC.Inv+"Inv "+TC.Rst+TC.Reg+"Reg "+TC.Rst+TC.RegF+"RegF "+TC.Rst+TC.RegB+"RegB")
+  return
