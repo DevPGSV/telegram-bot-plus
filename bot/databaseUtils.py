cc716cff3efc472872da2a50739ab96528efa075
DevPGSV <DevPGSV@gmail.com>
2015-05-31 18:15:07 +0200

    Uploaded databaseUtils file

---
+++ bot/databaseUtils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+class user
+  def add(user):
+    return
+  def update(user, changes):
+    return
+  def delete(user):
+    return
+  def existsInDB(user):
+    return true
+
+class chat
+  def add(chat):
+    return
+  def update(chat, changes):
+    return
+  def delete(chat):
+    return
+  def existsInDB(chat):
+    return true
+  def addMember(user):
+    return
+  def deleteMember(user):
+    return
+  def isMember(user):
+    return true
+
