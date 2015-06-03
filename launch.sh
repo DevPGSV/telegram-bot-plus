8a2453f7fa4cc1df45acae555d3548934718947c
DevPGSV <DevPGSV@gmail.com>
2015-06-03 17:44:12 +0200

    Added parameters to tg-cli

--- launch.sh
+++ launch.sh
@@ -13,4 +13,5 @@ if [ ! -f ./tg/bin/telegram-cli ]; then
   exit 1
 fi
 
-./tg/bin/telegram-cli -k ./tg/tg-server.pub -Z ./bot/launcher.py -W -l 0 $@
+fuser 8080/tcp && (echo "Port is being used"; exit 1;)
+./tg/bin/telegram-cli -k ./tg/tg-server.pub -Z ./bot/launcher.py -W -l 0 -D -R -P 8080 --json $@
